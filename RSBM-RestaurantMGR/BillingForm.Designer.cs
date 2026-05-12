feddad1a6f79d29dbf4c5832cd0c6d336fbcfa9f
Doomslayer1001 <danushsoosai28@gmail.com>
2026-05-12 02:18:07 -0400

    remove the  foreign key

--- RSBM-RestaurantMGR/BillingForm.Designer.cs
+++ RSBM-RestaurantMGR/BillingForm.Designer.cs
@@ -346,6 +346,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(526, 693);
             this.Controls.Add(this.dataGridBill);
